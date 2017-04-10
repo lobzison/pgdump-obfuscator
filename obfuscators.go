7f893d133fdddda42fdb5a55daf4fd7ad1511f37
Georgiy Kutsurua <g.kutsurua@gmail.com>
2017-04-10 19:36:38 +0400

    Add unique email generator Add safe bytes support

--- obfuscators.go
+++ obfuscators.go
@@ -6,14 +6,15 @@ import (
 	"bytes"
 	"crypto/rand"
 	"crypto/sha256"
-	"net"
 	"errors"
 	"fmt"
+	"net"
 )
 
 var Salt []byte
 
 const emailDomain = "@example.com"
+const DomainLen = 10
 
 func GenScrambleBytes(maxLength uint) func([]byte) []byte {
 	return func(s []byte) []byte {
@@ -23,8 +24,11 @@ func GenScrambleBytes(maxLength uint) func([]byte) []byte {
 }
 
 var bytesOutputAlphabetLength = byte(len(bytesOutputAlphabet))
+var bytesSafeAlphabetLength = byte(len(bytesSafeAlphabet))
 var bytesKeep = []byte("',\\{}")
 var bytesOutputAlphabet = []byte("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+-_")
+var bytesSafeAlphabet = []byte("0123456789abcdefghijklmnopqrstuvwxyz")
+var DomainPostfix = []byte(".example")
 
 // Modifies `s` in-place.
 func ScrambleBytes(s []byte) []byte {
@@ -49,6 +53,37 @@ func ScrambleBytes(s []byte) []byte {
 		if !isArray || bytes.IndexRune(bytesKeep, r) == -1 {
 			// Do not insert, so should not obstruct reader.
 			s[i] = bytesOutputAlphabet[(sumBytes[i%sumLength]+byte(r))%bytesOutputAlphabetLength]
+		} else {
+			// Possibly shift bytes to beginning of s.
+			s[i] = byte(r)
+		}
+	}
+	return s
+}
+
+// Modifies `s` in-place.
+func ScrambleSafeBytes(s []byte) []byte {
+	isArray := len(s) >= 2 && s[0] == '{' && s[len(s)-1] == '}'
+
+	hash := sha256.New()
+	// Hard-coding this constant wins less than 3% in BenchmarkScrambleBytes
+	const sumLength = 32 // SHA256/8
+	hash.Write(Salt)
+	hash.Write(s)
+	sumBytes := hash.Sum(nil)
+
+	reader := bytes.NewReader(s)
+	var r rune
+	var err error
+	for i := 0; ; i++ {
+		r, _, err = reader.ReadRune()
+		if err != nil {
+			s = s[:i]
+			break
+		}
+		if !isArray || bytes.IndexRune(bytesKeep, r) == -1 {
+			// Do not insert, so should not obstruct reader.
+			s[i] = bytesSafeAlphabet[(sumBytes[i%sumLength]+byte(r))%bytesSafeAlphabetLength]
 		} else {
 			// Possibly shift bytes to beginning of s.
 			s[i] = byte(r)
@@ -87,6 +122,24 @@ func scrambleOneEmail(s []byte) []byte {
 	return s[:len(mailbox)+len(emailDomain)]
 }
 
+func scrambleOneUniqueEmail(s []byte) []byte {
+	atIndex := bytes.IndexRune(s, '@')
+	mailbox := Salt
+	if atIndex != -1 {
+		mailbox = s[:atIndex]
+	}
+	total := len(mailbox) + 1 + DomainLen + len(DomainPostfix)
+	s = make([]byte, total)
+	copy(s, mailbox)
+	// ScrambleBytes is in-place, but may return string shorter than input.
+	mailbox = ScrambleBytes(s[:len(mailbox)])
+	ScrambleSafeBytes(s[len(mailbox)+1 : len(mailbox)+1+DomainLen])
+	copy(s[len(mailbox):], []byte("@"))
+	copy(s[len(mailbox)+DomainLen+1:], DomainPostfix)
+	// So final len(mailbox) may be shorter than whole allocated string.
+	return s[:total]
+}
+
 // Supports array of emails in format {email1,email2}
 func ScrambleEmail(s []byte) []byte {
 	if len(s) < 2 {
@@ -101,6 +154,29 @@ func ScrambleEmail(s []byte) []byte {
 	outLength := 2 + len(parts) - 1
 	for i, bs := range parts {
 		partsNew[i] = scrambleOneEmail(bs)
+		outLength += len(partsNew[i])
+	}
+	s = make([]byte, outLength)
+	s[0] = '{'
+	s[len(s)-1] = '}'
+	copy(s[1:len(s)-1], bytes.Join(partsNew, []byte{','}))
+	return s
+}
+
+// Supports array of emails in format {email1,email2}
+func ScrambleUniqueEmail(s []byte) []byte {
+	if len(s) < 2 {
+		// panic("ScrambleEmail: input is too small: '" + string(s) + "'")
+		return s
+	}
+	if s[0] != '{' && s[len(s)-1] != '}' {
+		return scrambleOneUniqueEmail(s)
+	}
+	parts := bytes.Split(s[1:len(s)-1], []byte{','})
+	partsNew := make([][]byte, len(parts))
+	outLength := 2 + len(parts) - 1
+	for i, bs := range parts {
+		partsNew[i] = scrambleOneUniqueEmail(bs)
 		outLength += len(partsNew[i])
 	}
 	s = make([]byte, outLength)
@@ -130,14 +206,18 @@ func ScrambleInet(s []byte) []byte {
 
 func GetScrambleByName(value string) (func(s []byte) []byte, error) {
 	switch value {
-		case "bytes":
-			return ScrambleBytes, nil
-		case "digits":
-			return ScrambleDigits, nil
-		case "email":
-			return ScrambleEmail, nil
-		case "inet":
-			return ScrambleInet, nil
+	case "bytes":
+		return ScrambleBytes, nil
+	case "sbytes":
+		return ScrambleSafeBytes, nil
+	case "digits":
+		return ScrambleDigits, nil
+	case "email":
+		return ScrambleEmail, nil
+	case "uemail":
+		return ScrambleUniqueEmail, nil
+	case "inet":
+		return ScrambleInet, nil
 	}
 	return nil, errors.New(fmt.Sprintf("%s is not registered scramble function", value))
 }
