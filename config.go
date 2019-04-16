3fc351861e7ab36a2c6b742b369997acbcd1d501
Yurii Danyliak <yurii.d@matic.com>
2019-04-16 11:02:59 +0300

    Update config.go

--- config.go
+++ config.go
@@ -8,38 +8,128 @@ type Configuration struct {
 var Config *Configuration = &Configuration{
 	Obfuscations: []TargetedObfuscation{
 		TargetedObfuscation{
-			Target{Table: "auth_user", Column: "email"},
-			ScrambleEmail,
+			Target{Table: "leads", Column: "tsv"},
+			ScrambleBytes,
+		},
+		
+		
+		TargetedObfuscation{
+			Target{Table: "customers", Column: "first_name"},
+			ScrambleBytes,
 		},
 		TargetedObfuscation{
-			Target{Table: "auth_user", Column: "password"},
+			Target{Table: "customers", Column: "last_name"},
+			ScrambleBytes,
+		},
+		TargetedObfuscation{
+			Target{Table: "customers", Column: "middle_name"},
+			ScrambleBytes,
+		},
+		TargetedObfuscation{
+			Target{Table: "customers", Column: "email"},
+			ScrambleUniqueEmail,
+		},
+		TargetedObfuscation{
+			Target{Table: "customers", Column: "hashed_email"},
 			ScrambleBytes,
 		},
 
-        	// address_useraddress
+		
 		TargetedObfuscation{
-			Target{Table: "address_useraddress", Column: "phone_number"},
-			ScrambleDigits,
+			Target{Table: "customer_addresses", Column: "address_line1"},
+			ScrambleBytes,
 		},
 		TargetedObfuscation{
-			Target{Table: "address_useraddress", Column: "land_line"},
-			ScrambleDigits,
+			Target{Table: "customer_addresses", Column: "address_line2"},
+			ScrambleBytes,
 		},
 
-		// order_order
+		
 		TargetedObfuscation{
-			Target{Table: "order_order", Column: "guest_email"},
+			Target{Table: "customer_details", Column: "date_of_birth"},
+			ScrambleDigits,
+		},
+		TargetedObfuscation{
+			Target{Table: "customer_details", Column: "fico_score"},
+			ScrambleDigits,
+		},
+		TargetedObfuscation{
+			Target{Table: "customer_details", Column: "phone"},
+			ScrambleDigits,
+		},
+		TargetedObfuscation{
+			Target{Table: "customer_details", Column: "business_phone"},
+			ScrambleDigits,
+		},
+		TargetedObfuscation{
+			Target{Table: "customer_details", Column: "ssn"},
+			ScrambleDigits,
+		},
+		TargetedObfuscation{
+			Target{Table: "customer_details", Column: "short_ssn"},
+			ScrambleBytes,
+		},
+
+		
+		TargetedObfuscation{
+			Target{Table: "loans", Column: "lender_name"},
+			ScrambleBytes,
+		},
+		TargetedObfuscation{
+			Target{Table: "loans", Column: "amount"},
+			ScrambleDigits,
+		},
+		TargetedObfuscation{
+			Target{Table: "loans", Column: "number"},
+			ScrambleBytes,
+		},
+		TargetedObfuscation{
+			Target{Table: "loans", Column: "closing_date"},
+			ScrambleDigits,
+		},
+		
+		
+		TargetedObfuscation{
+			Target{Table: "coborrowers", Column: "name"},
+			ScrambleBytes,
+		},
+		TargetedObfuscation{
+			Target{Table: "coborrowers", Column: "email"},
 			ScrambleEmail,
 		},
-
-		// order_shippingaddress
 		TargetedObfuscation{
-			Target{Table: "order_shippingaddress", Column: "phone_number"},
+			Target{Table: "coborrowers", Column: "phone"},
 			ScrambleDigits,
 		},
 		TargetedObfuscation{
-			Target{Table: "order_shippingaddress", Column: "land_line"},
+			Target{Table: "coborrowers", Column: "business_phone"},
 			ScrambleDigits,
 		},
+		TargetedObfuscation{
+			Target{Table: "coborrowers", Column: "date_of_birth"},
+			ScrambleDigits,
+		},
+		TargetedObfuscation{
+			Target{Table: "coborrowers", Column: "ssn"},
+			ScrambleDigits,
+		},
+		
+		
+		TargetedObfuscation{
+			Target{Table: "policies", Column: "company"},
+			ScrambleBytes,
+		},
+		TargetedObfuscation{
+			Target{Table: "policies", Column: "expiration_date"},
+			ScrambleDigits,
+		},
+		TargetedObfuscation{
+			Target{Table: "policies", Column: "number"},
+			ScrambleBytes,
+		},
+		TargetedObfuscation{
+			Target{Table: "policies", Column: "normalized_company"},
+			ScrambleBytes,
+		}
 	},
 }
